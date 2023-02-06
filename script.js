8394853edbe03bffd188e79e67cc2f3313846d77
felicia <feli5659@stud.kea.dk>
2023-02-06 12:59:33 +0100

    add rockpaperscissors to exercises folder

--- script.js
+++ script.js
@@ -13,35 +13,42 @@ function start() {
   console.log("loaded");
 }
 
-// when rock-button is clicked it defines the variable userChoice as rock and runs functions userchooses and random number
+// when rock-button is clicked it defines the variable userChoice as rock
 rock.addEventListener("click", function () {
   userChoice = "rock";
+  //   remove classes and add "hidden" text
   document.querySelector("#player2").classList.remove("paper", "scissors", "rock");
   document.querySelector("#player1").classList.remove("paper", "scissors", "rock");
   document.querySelector("#win").classList.add("hidden");
   document.querySelector("#lose").classList.add("hidden");
   document.querySelector("#draw").classList.add("hidden");
+  //   add animations and when animation ends player 1 goes to function userChooses
   document.querySelector("#player1").classList.add("shake");
   document.querySelector("#player1").addEventListener("animationend", userChooses);
+  //   add animations and when animation ends player 2 goes to function randomNum
   document.querySelector("#player2").classList.add("shake");
   document.querySelector("#player2").addEventListener("animationend", randomNum);
 });
-// when paper-button is clicked it defines the variable userChoice as paper and runs functions userchooses and random number
+
+// when paper-button is clicked it defines the variable userChoice as paper
 
 paper.addEventListener("click", function () {
   userChoice = "paper";
+  //   remove classes and add "hidden" text
   document.querySelector("#player2").classList.remove("paper", "scissors", "rock");
   document.querySelector("#player1").classList.remove("paper", "scissors", "rock");
   document.querySelector("#win").classList.add("hidden");
   document.querySelector("#lose").classList.add("hidden");
   document.querySelector("#draw").classList.add("hidden");
+  //   add animations and when animation ends player 1 goes to function userChooses
   document.querySelector("#player1").classList.add("shake");
   document.querySelector("#player1").addEventListener("animationend", userChooses);
+  //   add animations and when animation ends player 2 goes to function randomNum
   document.querySelector("#player2").classList.add("shake");
   document.querySelector("#player2").addEventListener("animationend", randomNum);
 });
 
-// when scissors-button is clicked it defines the variable userChoice as scissors and runs functions userchooses and random number
+// when scissor-button is clicked it defines the variable userChoice as scissors
 
 scissors.addEventListener("click", function () {
   userChoice = "scissors";
@@ -50,14 +57,16 @@ scissors.addEventListener("click", function () {
   document.querySelector("#win").classList.add("hidden");
   document.querySelector("#lose").classList.add("hidden");
   document.querySelector("#draw").classList.add("hidden");
+  //   add animations and when animation ends player 1 goes to function userChooses
   document.querySelector("#player1").classList.add("shake");
   document.querySelector("#player1").addEventListener("animationend", userChooses);
+  //   add animations and when animation ends player 2 goes to function randomNum
   document.querySelector("#player2").classList.add("shake");
   document.querySelector("#player2").addEventListener("animationend", randomNum);
 });
 
 function userChooses() {
-  // if statements that declare in the console what the user has chosen
+  // if statements that depending on choice, it stops animation shake and adds picture for the choice - rock paper or scissors
   if (userChoice == "rock") {
     document.querySelector("#player1").classList.remove("shake");
     document.querySelector("#player1").classList.add("rock");
@@ -75,9 +84,9 @@ function userChooses() {
 function randomNum() {
   compNum = Math.floor(Math.random() * 3) + 1;
   computerChooses();
-  // now add eventlistener that when animation is finished go to function computerChooses
 }
 
+// if statements that, depending on choice, it stops animation shake and adds picture for the choice - rock paper or scissors
 function computerChooses() {
   if (compNum == 1) {
     compChoice = "rock";
